be775733fa501da548c4f5f4a0663751082cfb95
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-04 11:33:18 -0700

    tested get all function and coded it

--- test.js/store.test.js
+++ test.js/store.test.js
@@ -4,11 +4,24 @@ const Store = require ('../lib/store');
 
 describe ('tests for store class', () => {
     
+    const store = new Store();
+    let id;
     
     it ('test to save to store', () => {
-        const store = new Store();
         const toSave = store.save({ favFood: 'tacos' });
+        id = toSave._id;
         assert.ok(toSave._id);
+        assert.equal(toSave.favFood, 'tacos');
+    });
+    it ('test to get all', () => {
+        const toSave = store.save({ favFood: 'chocolate' });
+        const allData = store.getAll();
+        assert.equal(allData.length, 2);
+        
+    });
+    it ('test to get id', () => {
+        const result = store.get(id);
+        assert.equal(result._id, id);
     });
 
 });
