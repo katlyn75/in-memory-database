06b3af16c1c18c599ab2efc47c4d8efb539165d6
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-04 09:47:51 -0700

    renamed folders and files, added to store

--- store.js
+++ store.js
@@ -1,9 +1,22 @@
-//placeholder
+const store = new Store();
 
-users.insert({
-    _id: shortid.generate()
-    name: ...
-    email: ...
-    });
+const toSave = store.save({ name: 'katlyn', favFood: 'tacos', favHoliday: 'Halloween'};
+const katlyn = store.save(toSave);
+//{ _shortid: 789, name 'katlyn', favFood 'tacos', favHoliday 'Halloween'}
 
-shortid.isValid ('not a number id');
+const got = store.get(katlyn.id);
+assert.deepEqual(katlyn, got);
+
+store.save({name: 'valerie', favFood: 'tacos', favHoliday: 'Christmas'});
+
+store.getAll();
+
+//expect this to return 
+// [
+//    {name 'katlyn', favFood: 'tacos', favHoliday: 'Halloween'},
+//    {name 'valerie, favFood: 'tacos', favHoliday: 'Christmas'}
+// ]
+
+
+
+
