ad9277a1c789b1f73ee17988bfaa1010654995ed
Katlyn Tucker <studioquirk@yahoo.com>
2018-04-09 01:05:47 -0700

    unsuccessfully writing last test for remove

--- test.js/store.test.js
+++ test.js/store.test.js
@@ -5,24 +5,28 @@ const Store = require ('../lib/store');
 describe ('tests for store class', () => {
     
     const store = new Store();
-    // let id;
     
     it ('test to save to store', () => {
         const toSave = store.save({ favFood: 'tacos' });
-        //let id = toSave._id;
-        assert.ok(toSave._id);
+        let id = toSave._id;
+        assert.ok(id);
         assert.equal(toSave.favFood, 'tacos');
     });
     it ('test to get all', () => {
         store.save({ favFood: 'chocolate' });
         const allData = store.getAll();
         assert.equal(allData.length, 2);
-        
     });
     it ('test to get id', () => {
         const toSave = store.save({ favFood: 'chocolate' });
         const result = store.get(toSave._id);
         assert.deepEqual(toSave, result);
+    });
+    it ('test to remove id', () => {
+        const result = store.get(result);
+        const removed = store.remove(result);//remove the obj with that id
+        assert.equal(removed, true);// if removed, return true
+        assert.equal(removed, false);// if no id exists, return false
     });
 
 });
